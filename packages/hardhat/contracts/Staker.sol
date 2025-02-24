3fbaef0b24cbb820b1b03ba31047e002918a5a35
vxxxxc <>
2025-02-25 02:21:18 +0800

    implement more logic to handle bad UX

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -7,8 +7,15 @@ import "./ExampleExternalContract.sol";
 contract Staker {
     ExampleExternalContract public exampleExternalContract;
     mapping(address => uint256) public balance;
+    mapping(address => bool) withdrawal;
+    mapping(address => bool) staked;
     bool public executed = false;
-    bool public withdrawed = false;
+
+    struct Stakers {
+        address stakerAddress;
+    }
+
+    Stakers[] stakers;
 
     uint256 public constant threshold = 1 ether;
     uint256 public deadline = block.timestamp + 30 seconds;
@@ -21,6 +28,11 @@ contract Staker {
         require(msg.value > 0.1 ether, "You must at least send 0.1 ETH !!");
         balance[address(this)] += msg.value;
         emit Stake(msg.sender, msg.value);
+        if (!checkStaker(msg.sender)) {
+            stakers.push(Stakers(msg.sender));
+        }
+        withdrawal[msg.sender] = false;
+        staked[msg.sender] = true;
     }
 
     event Stake(address, uint256);
@@ -29,13 +41,17 @@ contract Staker {
         require(!executed, "Already executed..");
         require(balance[address(this)] >= threshold, "Balance threshold below 1 ETH..");
         exampleExternalContract.complete{ value: address(this).balance }();
+        executed = true;
     }
 
     function withdraw() external expireDeadline {
-        require(!withdrawed, "Already withdrawed..");
+        require(checkStaker(msg.sender), "You are not staked yet...");
+        require(!withdrawal[msg.sender], "Already withdrawed..");
         require(balance[address(this)] < threshold, "Contract balance over threshold!");
         (bool s, ) = msg.sender.call{ value: balance[address(this)] }("");
         require(s);
+        withdrawal[msg.sender] = true;
+        staked[msg.sender] = false;
     }
 
     modifier expireDeadline() {
@@ -53,6 +69,15 @@ contract Staker {
 
     receive() external payable {
         this.stake();
+    }
+
+    function checkStaker(address input) internal view returns (bool) {
+        for (uint i = 0; i < stakers.length; i++) {
+            if (stakers[i].stakerAddress == input) {
+                return true;
+            }
+        }
+        return false;
     }
 
     // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
--- packages/nextjs/contracts/deployedContracts.ts
+++ packages/nextjs/contracts/deployedContracts.ts
@@ -7,7 +7,7 @@ import { GenericContractsDeclaration } from "~~/utils/scaffold-eth/contract";
 const deployedContracts = {
   31337: {
     ExampleExternalContract: {
-      address: "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9",
+      address: "0x4826533B4897376654Bb4d4AD88B7faFD0C98528",
       abi: [
         {
           inputs: [],
@@ -33,7 +33,7 @@ const deployedContracts = {
       inheritedFunctions: {},
     },
     Staker: {
-      address: "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707",
+      address: "0x99bbA657f2BbC93c02D617f8bA121cB8Fc104Acf",
       abi: [
         {
           inputs: [
@@ -115,6 +115,19 @@ const deployedContracts = {
           name: "execute",
           outputs: [],
           stateMutability: "nonpayable",
+          type: "function",
+        },
+        {
+          inputs: [],
+          name: "executed",
+          outputs: [
+            {
+              internalType: "bool",
+              name: "",
+              type: "bool",
+            },
+          ],
+          stateMutability: "view",
           type: "function",
         },
         {
